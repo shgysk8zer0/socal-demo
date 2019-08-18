29879bd775d0dd003fd542652c97e1b07e197b9e
Chris Zuber <shgysk8zer0@gmail.com>
2019-08-18 16:13:50 -0700

    Do not set value for adult/children

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const config = {
-	version: location.hostname === 'localhost' ? new Date().toISOString() : '1.0.0-a9',
+	version: location.hostname === 'localhost' ? new Date().toISOString() : '1.0.0-a10',
 	stale: [
 		/* Root document */
 		'/',
