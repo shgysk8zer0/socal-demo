1b498ce9feb6c2ce63ac17fa30842b35dbd5c713
Chris Zuber <shgysk8zer0@gmail.com>
2019-08-18 15:46:01 -0700

    Fix toast sizing

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const config = {
-	version: location.hostname === 'localhost' ? new Date().toISOString() : '1.0.0-a8',
+	version: location.hostname === 'localhost' ? new Date().toISOString() : '1.0.0-a9',
 	stale: [
 		/* Root document */
 		'/',
