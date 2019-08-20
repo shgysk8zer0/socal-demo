a2da72f932fa4179880a92881513b1d382c7ce21
Chris Zuber <shgysk8zer0@gmail.com>
2019-08-20 11:42:53 -0700

    Misc. enhancemnts
    - Update to be `https://schema.org/Trip` for data
    - Show date and time inputs
    - Show data on submit in a `<toast-message>`

--- js/offer-catelog.js
+++ js/offer-catelog.js
@@ -6,17 +6,20 @@ class OfferCatelogElement extends HTMLElement {
 		await customElements.whenDefined('rafting-trip');
 		const RaftingTripElement = customElements.get('rafting-trip');
 		const trips = await resp.json();
-		const els = trips.map(trip => {
+		const els = await Promise.all(trips.map(async trip => {
 			const el = new RaftingTripElement();
+			await el.ready;
 			const adult = trip.offers.find(offer => offer.name.toLowerCase() === 'adult');
 			const child = trip.offers.find(offer => offer.name.toLowerCase() === 'child');
 			el.name = trip.name;
 			el.image = trip.image[2];
+			el.identifier = trip.identifier;
 			el.description = trip.description;
 			el.adultPrice = {value: adult.price, currency: adult.priceCurrency};
 			el.childPrice = {value: child.price, currency: child.priceCurrency};
+			el.departureTimes = trip.departureTime;
 			return el;
-		});
+		}));
 		this.append(...els);
 	}
 }
