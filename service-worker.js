67047b847ea6c459f70df2b249f9a7794712d604
Chris Zuber <shgysk8zer0@gmail.com>
2019-08-24 12:10:40 -0700

    Add structured data embedded in site
    Also hide social link labels on mobile

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const config = {
-	version: location.hostname === 'localhost' ? new Date().toISOString() : '1.0.0-a2',
+	version: location.hostname === 'localhost' ? new Date().toISOString() : '1.0.0-a3',
 	stale: [
 		/* Root document */
 		'/',
