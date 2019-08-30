e703c969301edf6d9feef5fc5d4c372c6106152e
Chris Zuber <shgysk8zer0@gmail.com>
2019-08-30 16:48:55 -0700

    Version bump service worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const config = {
-	version: location.hostname === 'localhost' ? new Date().toISOString() : '1.0.0-a7',
+	version: location.hostname === 'localhost' ? new Date().toISOString() : '1.0.0-a8',
 	stale: [
 		/* Root document */
 		'/',
